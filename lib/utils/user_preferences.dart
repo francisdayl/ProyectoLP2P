6f15e61ef140d641be81c84043844c839ea7a817
Marco Del Rosario <marsdel@espol.edu.ec>
2021-08-31 22:37:53 -0500

    perfil de usuario

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:proyecto2p/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+    name: 'Samira Suarez',
+    email: 'samsur09@gmail.com',
+    about:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+  );
+}
